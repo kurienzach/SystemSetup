7982143daf25fcd011822ee1e59d1dfd3e651d97
Kurien Zacharia <kurienzach@gmail.com>
2017-06-30 16:53:40 +0530

    Added installation for google-chrome, vim, zsh

---
+++ packages/google-chrome.sh
@@ -0,0 +1,6 @@
+# Download latest version of google-chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
+
---
+++ packages/zsh.sh
@@ -0,0 +1,8 @@
+# Install zsh
+sudo apt-get install zsh
+
+# Chanage shell 
+chsh -s $(which zsh)
+
+# Logout
+gnome-session-quit
