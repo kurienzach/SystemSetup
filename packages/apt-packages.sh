f34bf41f58c2a0fd9f24b7dc82d798c444258570
Kurien Zacharia <kurienzach@gmail.com>
2017-06-30 17:24:28 +0530

    Added antigen install and dotfiles for zsh

--- packages/apt-packages.sh
+++ packages/apt-packages.sh
@@ -1,1 +1,10 @@
 apt-get -y install git
+apt-get -y install curl
+
+# Hack font
+apt-get -y install fonts-hack-ttf
+
+# Install gnu stow for managing dot files
+apt-get -y install stow
+
+apt-get -y install tmux
